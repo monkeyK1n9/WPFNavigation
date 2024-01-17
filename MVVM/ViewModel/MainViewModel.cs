ea6e70974a0367176f18e4aa5eba2bf724922e7a
monkeyK1n9 <mkeyk1n9@gmail.com>
2024-01-17 14:22:14 +0100

    implement navigation commands in mainviewmodel

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPFNavigation.Core;
+using WPFNavigation.Services;
 
 namespace WPFNavigation.MVVM.ViewModel
 {
     public class MainViewModel : Core.ViewModel
     {
+        private INavigationService _navigationService;
+        public INavigationService NavigationService
+        {
+            get { return _navigationService; }
+            set
+            {
+                _navigationService = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand NavigateToHomeCommand { get; set; }
+        public RelayCommand NavigateToSettingsCommand { get; set; }
+        public MainViewModel(INavigationService navigationService)
+        {
+            NavigationService = navigationService;
+            NavigateToHomeCommand = new RelayCommand(execute: o => { NavigationService.NavigateTo<HomeViewModel>(); }, canExecute: o => true);
+            NavigateToSettingsCommand = new RelayCommand(execute: o => { NavigationService.NavigateTo<SettingsViewModel>(); }, canExecute: o => true);
+        }
     }
 }
